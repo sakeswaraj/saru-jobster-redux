f43e071b643c8c3d965526323413794b4188b5d8
Sake Swaraj <sake.swaraj@gobolt.co.in>
2022-08-10 19:20:38 +0530

    error page setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import { Dashboard, Error, Landing, Register } from "./routes";
 function App() {
   return (
     <div className="pt-6">
-      <Landing />
-      <Register />
-      <Error />
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
   );
 }
